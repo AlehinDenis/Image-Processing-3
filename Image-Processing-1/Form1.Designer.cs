694c9d670481f7d8f0e7bbf95886672d7ab1921f
Denis <alehin.de@gmail.com>
2020-12-17 17:56:34 +0300

    1

---
+++ Image-Processing-1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Image_Processing_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.filtersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNoiseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeNoiseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filtersToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // filtersToolStripMenuItem
+            // 
+            this.filtersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNoiseToolStripMenuItem,
+            this.removeNoiseToolStripMenuItem});
+            this.filtersToolStripMenuItem.Name = "filtersToolStripMenuItem";
+            this.filtersToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.filtersToolStripMenuItem.Text = "Filters";
+            // 
+            // addNoiseToolStripMenuItem
+            // 
+            this.addNoiseToolStripMenuItem.Name = "addNoiseToolStripMenuItem";
+            this.addNoiseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addNoiseToolStripMenuItem.Text = "Fourier transform";
+            this.addNoiseToolStripMenuItem.Click += new System.EventHandler(this.addNoiseToolStripMenuItem_Click);
+            // 
+            // removeNoiseToolStripMenuItem
+            // 
+            this.removeNoiseToolStripMenuItem.Name = "removeNoiseToolStripMenuItem";
+            this.removeNoiseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.removeNoiseToolStripMenuItem.Text = "Frequency filtering";
+            this.removeNoiseToolStripMenuItem.Click += new System.EventHandler(this.removeNoiseToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(380, 411);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(408, 55);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(380, 411);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(408, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 469);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Image Processing";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem filtersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNoiseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeNoiseToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Image-Processing-1/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace Image_Processing_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap image1, image2;
+        int[,] edge;
+
+        int Clamp(int value, int min, int max)
+        {
+            if (value < min) return min;
+            if (value > max) return max;
+            return value;
+        }
+
+        public void GrayScaleFilter()
+        {
+            for (int i = 0; i < image1.Width; i++)
+            {
+                for (int j = 0; j < image1.Height; j++)
+                {
+                    Color sourceColor = image1.GetPixel(i, j);
+                    int intensity = (int)(0.299 * sourceColor.R + 0.587 * sourceColor.G + 0.114 * sourceColor.B);
+                    Color color = Color.FromArgb(intensity, intensity, intensity);
+                    image1.SetPixel(i, j, color);
+                }
+            }
+        }
+
+        public void MedianFilter()
+        {
+            for (int i = 0; i < image1.Width; i++)
+            {
+                for (int j = 0; j < image1.Height; j++)
+                {
+                    int[] medianR = new int[9];
+                    int[] medianG = new int[9];
+                    int[] medianB = new int[9];
+
+                    int p = 0;
+                    for (int l = -1; l <= 1; l++)
+                        for (int k = -1; k <= 1; k++)
+                        {
+                            int idX = Clamp(i + k, 0, image1.Width - 1);
+                            int idY = Clamp(j + l, 0, image1.Height - 1);
+
+                            medianR[p] = image1.GetPixel(idX, idY).R;
+                            medianG[p] = image1.GetPixel(idX, idY).G;
+                            medianB[p++] = image1.GetPixel(idX, idY).B;
+                        }
+                    Array.Sort(medianR);
+                    Array.Sort(medianG);
+                    Array.Sort(medianB);
+
+                    Color color = Color.FromArgb(
+                        Clamp(medianR[4], 0, 255),
+                        Clamp(medianG[4], 0, 255),
+                        Clamp(medianB[4], 0, 255));
+                    image1.SetPixel(i, j, color);
+                }
+            }
+        }
+  
+        public void SobelFilter()
+        {
+            const int size = 3;
+            float[,] kernel = new float[size, size] { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
+            float[,] kernelY = new float[size, size] { { -1, -2, -1 }, { 0, 0, 0 }, { 1, 2, 1 } };
+
+            for (int i = 0; i < image1.Width; i++)
+            {
+                for (int j = 0; j < image1.Height; j++)
+                {
+                    int radiusX = kernel.GetLength(0) / 2;
+                    int radiusY = kernel.GetLength(1) / 2;
+                    float resultR = 0;
+                    float resultG = 0;
+                    float resultB = 0;
+                    float gradientX = 0;
+                    float gradientY = 0;
+                    for (int l = -radiusY; l <= radiusY; l++)
+                        for (int k = -radiusX; k <= radiusX; k++)
+                        {
+                            int idX = Clamp(i + k, 0, image1.Width - 1);
+                            int idY = Clamp(j + l, 0, image1.Height - 1);
+                            Color neighborColor = image1.GetPixel(idX, idY);
+                            resultR += neighborColor.R * kernel[k + radiusX, l + radiusY] + neighborColor.R * kernelY[k + radiusX, l + radiusY];
+                            resultG += neighborColor.G * kernel[k + radiusX, l + radiusY] + neighborColor.G * kernelY[k + radiusX, l + radiusY];
+                            resultB += neighborColor.B * kernel[k + radiusX, l + radiusY] + neighborColor.B * kernelY[k + radiusX, l + radiusY];
+                            gradientX += kernel[k + radiusX, l + radiusY] * (neighborColor.R *  + neighborColor.G * neighborColor.B);
+                            gradientY += kernelY[k + radiusX, l + radiusY] * (neighborColor.R * + neighborColor.G * neighborColor.B);
+                        }
+                    Color color = Color.FromArgb(
+                        Clamp((int)resultR, 0, 255),
+                        Clamp((int)resultG, 0, 255),
+                        Clamp((int)resultB, 0, 255));
+                    image2.SetPixel(i, j, color);
+
+                    if (gradientX == 0)
+                        edge[i, j] = 90;
+                    else
+                        edge[i, j] = (int)(Math.Atan(gradientY / gradientX) * 180 / Math.PI);
+                }
+            }
+        }
+
+        public void noneMax()
+        {
+            for (int i = 1; i < image2.Width - 1; i++)
+            {
+                for (int j = 1; j < image2.Height - 1; j++)
+                {
+                    float gradient = edge[i, j];
+                    if ((-22.5 < gradient && gradient <= 22.5) || (157.5 < gradient && gradient <= -157.5))
+                    {
+                        int intens1 = image2.GetPixel(i, j).R + image2.GetPixel(i, j).G + image2.GetPixel(i, j).B;
+                        int intens2 = image2.GetPixel(i, j + 1).R + image2.GetPixel(i, j + 1).G + image2.GetPixel(i, j + 1).B;
+                        int intens3 = image2.GetPixel(i, j - 1).R + image2.GetPixel(i, j - 1).G + image2.GetPixel(i, j - 1).B;
+                        if ((intens1 < intens2) || (intens1 < intens3))
+                            image1.SetPixel(i - 1, j - 1, Color.FromArgb(0, 0, 0));
+                        else
+                            image1.SetPixel(i, j, image2.GetPixel(i, j));
+                    }
+                    
+                    if ((-112.5 < gradient && gradient <= -67.5) || (67.5 < gradient && gradient <= 112.5))
+                    {
+                        int intens1 = image2.GetPixel(i, j).R + image2.GetPixel(i, j).G + image2.GetPixel(i, j).B;
+                        int intens2 = image2.GetPixel(i + 1, j).R + image2.GetPixel(i + 1, j).G + image2.GetPixel(i + 1, j).B;
+                        int intens3 = image2.GetPixel(i - 1, j).R + image2.GetPixel(i - 1, j).G + image2.GetPixel(i - 1, j).B;
+                        if ((intens1 < intens2) || (intens1 < intens3))
+                            image1.SetPixel(i - 1, j - 1, Color.FromArgb(0, 0, 0));
+                        else
+                            image1.SetPixel(i, j, image2.GetPixel(i, j));
+                    }
+
+                    if ((-67.5 < gradient && gradient <= -22.5) || (112.5 < gradient && gradient <= 157.5))
+                    {
+                        int intens1 = image2.GetPixel(i, j).R + image2.GetPixel(i, j).G + image2.GetPixel(i, j).B;
+                        int intens2 = image2.GetPixel(i - 1, j + 1).R + image2.GetPixel(i - 1, j + 1).G + image2.GetPixel(i - 1, j + 1).B;
+                        int intens3 = image2.GetPixel(i + 1, j - 1).R + image2.GetPixel(i + 1, j - 1).G + image2.GetPixel(i + 1, j - 1).B;
+                        if ((intens1 < intens2) || (intens1 < intens3))
+                            image1.SetPixel(i - 1, j - 1, Color.FromArgb(0, 0, 0));
+                        else
+                            image1.SetPixel(i, j, image2.GetPixel(i, j));
+                    }
+
+                    if ((-157.5 < gradient && gradient <= -112.5) || (22.5 < gradient && gradient <= 67.5))
+                    {
+                        int intens1 = image2.GetPixel(i, j).R + image2.GetPixel(i, j).G + image2.GetPixel(i, j).B;
+                        int intens2 = image2.GetPixel(i + 1, j + 1).R + image2.GetPixel(i + 1, j + 1).G + image2.GetPixel(i + 1, j + 1).B;
+                        int intens3 = image2.GetPixel(i - 1, j - 1).R + image2.GetPixel(i - 1, j - 1).G + image2.GetPixel(i - 1, j - 1).B;
+                        if ((intens1 < intens2) || (intens1 < intens3))
+                            image1.SetPixel(i - 1, j - 1, Color.FromArgb(0, 0, 0));
+                        else
+                            image1.SetPixel(i, j, image2.GetPixel(i, j));
+                    }
+                }
+            }
+        }
+
+        public void DualThresholdFiltering()
+        {
+            int down = 90;
+            int up = 120;
+            for (int i = 1; i < image1.Width - 1; i++)
+            {
+                for (int j = 1; j < image1.Height - 1; j++)
+                {
+                    if (image1.GetPixel(i, j).R > up && image1.GetPixel(i, j).G > up && image1.GetPixel(i, j).B > up)
+                        image2.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+                    else if (image1.GetPixel(i, j).R < down && image1.GetPixel(i, j).G < down && image1.GetPixel(i, j).B < down)
+                        image2.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+                    //else image2.SetPixel(i, j, Color.FromArgb(127, 127, 127));
+                } 
+            } 
+        }
+
+        private void addNoiseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog1 = new OpenFileDialog();
+            dialog1.Filter = "Image files|*.png;*.jpg;*.bmp|All filec(*.*)|*.*";
+            if (dialog1.ShowDialog() == DialogResult.OK)
+            {
+                image1 = new Bitmap(dialog1.FileName);
+                pictureBox1.Image = image1;
+                pictureBox1.Refresh();
+            }
+            else { return; }
+
+            image2 = new Bitmap(image1.Width,image1.Height);
+            edge = new int[image1.Width, image1.Height];
+
+            GrayScaleFilter();
+            MedianFilter();
+            SobelFilter();
+            noneMax();
+            DualThresholdFiltering();
+            pictureBox1.Image = image1;
+            pictureBox1.Refresh();
+            pictureBox2.Image = image2;
+            pictureBox2.Refresh();
+        }
+
+        private void removeNoiseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            label2.Visible = false;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+
+            OpenFileDialog dialog1 = new OpenFileDialog();
+            dialog1.Filter = "Image files|*.png;*.jpg;*.bmp|All filec(*.*)|*.*";
+            if (dialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Visible = true;
+                label1.Text = "Processing...";
+                image1 = new Bitmap(dialog1.FileName);
+                pictureBox1.Image = image1;
+                pictureBox1.Width = 380;
+                pictureBox1.Visible = true;
+                pictureBox1.Refresh();
+
+            }
+            else { return; }
+
+            if (dialog1.ShowDialog() == DialogResult.OK)
+            {
+                label2.Visible = true;
+                label2.Text = "Processing...";
+                image2 = new Bitmap(dialog1.FileName);
+                pictureBox2.Image = image2;
+                pictureBox2.Width = 380;
+                pictureBox2.Visible = true;
+                pictureBox2.Refresh();
+
+            }
+            else { return; }
+
+
+
+
+
+            int Clamp(int value, int min, int max)
+            {
+                if (value < min)
+                    return min;
+                if (value > max)
+                    return max;
+                return value;
+            }
+            //MyMedianFilter
+            int size = 3;
+            Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+            {
+                int[] medianR = new int[size * size];
+                int[] medianG = new int[size * size];
+                int[] medianB = new int[size * size];
+
+                int radius = size / 2;
+                int i = 0;
+                for (int l = -radius; l <= radius; l++)
+                    for (int k = -radius; k <= radius; k++)
+                    {
+                        int idX = Clamp(x + k, 0, sourceImage.Width - 1);
+                        int idY = Clamp(y + l, 0, sourceImage.Height - 1);
+
+                        medianR[i] = sourceImage.GetPixel(idX, idY).R;
+                        medianG[i] = sourceImage.GetPixel(idX, idY).G;
+                        medianB[i++] = sourceImage.GetPixel(idX, idY).B;
+                    }
+                Array.Sort(medianR);
+                Array.Sort(medianG);
+                Array.Sort(medianB);
+
+                return Color.FromArgb(
+                    Clamp(medianR[size * size / 2], 0, 255),
+                    Clamp(medianG[size * size / 2], 0, 255),
+                    Clamp(medianB[size * size / 2], 0, 255));
+            }
+
+
+            //Time start
+
+            var watch1 = System.Diagnostics.Stopwatch.StartNew();
+            //OpenCV Median filter
+            Mat srcImage = Cv2.ImRead(dialog1.FileName);
+            Mat filteredImage = new Mat();
+            Cv2.MedianBlur(srcImage, filteredImage, 3);
+            //Time end
+            watch1.Stop();
+            label2.Text = "OpenCV MedianFilter elapsed time: " + watch1.ElapsedMilliseconds.ToString() + "ms";
+
+            image2 = filteredImage.ToBitmap();
+            pictureBox2.Image = image2;
+            pictureBox2.Show();
+            pictureBox2.Refresh();
+
+            Bitmap resultImage = new Bitmap(image1.Width, image1.Height);
+            
+
+
+            //Time start
+            watch1 = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < image1.Width; i++)
+            {
+                for (int j = 0; j < image1.Height; j++)
+                {
+                    resultImage.SetPixel(i, j, calculateNewPixelColor(image1, i, j));
+                }
+            }
+            watch1.Stop();
+            //Time end
+            label1.Text = "MyMedianFilter elapsed time: " + watch1.ElapsedMilliseconds.ToString() + "ms";
+
+
+            pictureBox1.Image = resultImage;
+            pictureBox1.Refresh();
+
+        }
+
+    }
+    }
